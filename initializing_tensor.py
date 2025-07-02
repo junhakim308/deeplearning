c28b8a659d7fba3b913a90deb72e9fe1eacdb93a
junhakim308 <junha308@gmail.com>
2025-07-02 17:26:32 +0900

    change

---
+++ initializing_tensor.py
@@ -0,0 +1,7 @@
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+my tensor = torch.tensor([[1,2,3], [4,5,6]], dtype=torch.float32, device=device, requires_grad=True)
+
+print(my_tensor)
